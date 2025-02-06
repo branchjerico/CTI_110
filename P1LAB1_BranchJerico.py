ee493548e0af28cea065d8bb2754d1d837114c51
branchjerico <branchjerico@gmail.com>
2025-02-06 10:29:25 -0500

    Add files via upload

---
+++ P1LAB1_BranchJerico.py
@@ -0,0 +1,12 @@
+# Branch Jerico
+#2/4/2025
+#P1Lab1
+#Using the input ad print functions to display a name
+
+#Get user input for first name
+first_name = input("Enter your first name: ")
+
+last_name = input("Enter your last name:")
+
+#Display welcome message using the user
+print("Hello,", first_name, last_name +"!", "Welcome to CTI-110")
