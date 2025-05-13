5aaedfad6565d742dcb43af4c2bb69bb622ea3ea
branchjerico <branchjerico@gmail.com>
2025-05-12 20:08:30 -0400

    Add files via upload

---
+++ P1HW1.py
@@ -0,0 +1,33 @@
+# Jerico Branch
+ # 2/6/25
+ # P1HW1
+ # Mathematics
+
+ #Title of what we are doing"Calcuations of exponents"
+
+
+
+
+print("-" * 5, "Calucualing_exponents", "-" * 5)
+
+first_num = int(input("Enter an integer as the base value:"))
+second_num = int(input("Enter an integer as the exponent:"))
+
+exponent = first_num ** second_num
+
+print(first_num, "to the power of", second_num, "is", exponent, "!!!")
+
+print()
+print()
+print()
+#Part_2_of_the_homework
+
+
+print("-" * 5, "Addition and Subtraction", "-" * 5)
+
+first_num = int(input("Enter a starting integer:"))
+second_num = int(input("Enter an integer to add:"))
+third_num = int(input("Enter an integer to subtract:"))
+sum_of_num = first_num + second_num - third_num
+
+print("Sum of num:", first_num, "+",second_num, "-", third_num, "equal to", sum_of_num)
